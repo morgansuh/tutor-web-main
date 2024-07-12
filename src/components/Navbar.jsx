def02454c9451a0d46f6ca6c1c1130d26f0447b6
Morgan <mrgnsuh@gmail.com>
2024-07-12 15:06:54 -0400

    change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Logo from '../assets/logo.png'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
@@ -11,7 +12,8 @@ const Navbar = () => {
   return (
     <div className='bg-white w-full h-24'>
       <div className='flex justify-between items-center h-full max-w-[1240px] mx-auto px-4 text-black'>
-        <h1 className='text-3xl font-bold text-[#88cbea]'>LOGO.</h1>
+        {/* Apply a class to move the logo more to the left */}
+        <img className='custom-width mx-auto md:ml-0' src={Logo} alt="/" />
         <ul className='hidden md:flex'>
           <li className='p-4'>Home</li>
           <li className='p-4'>Pricing</li>
@@ -24,7 +26,7 @@ const Navbar = () => {
         </div>
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-white m-4'>LOGO.</h1>
+        <h1 className='w-full text-3xl font-bold text-white m-4'>PRIME TUTORING</h1>
         <li className='p-4 border-b border-gray-600 text-white'>Home</li>
         <li className='p-4 border-b border-gray-600 text-white'>Pricing</li>
         <li className='p-4 border-b border-gray-600 text-white'>Resources</li>
